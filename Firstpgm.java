12c159c0d6738481a5c92a6af3318b46fa1a78e8
ENG19CS0334-TEJAAL-M <eng19cs0334.tejaalm@gmail.com>
2023-02-04 19:56:32 +0530

    i have committed two pgms

---
+++ Firstpgm.java
@@ -0,0 +1,9 @@
+public class Firstpgm
+{
+		public static void main (String args[])
+		System.out.println("heyy rahul");
+	for(int i=0;i<10;i++)
+	{
+		System.out.println("good eveningg folkss");
+	}
+}
---
+++ Secondpgm.java
@@ -0,0 +1,9 @@
+public class Secondpgm
+{
+		public static void main (String args[])
+		System.out.println("second one");
+	for(int i=0;i<10;i++)
+	{
+		System.out.println("good eveningg folkss");
+	}
+}
