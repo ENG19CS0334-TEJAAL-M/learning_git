52f2263b2ac4630974c3965468db47b45e9faa46
ENG19CS0334-TEJAAL-M <eng19cs0334.tejaalm@gmail.com>
2023-02-04 20:25:40 +0530

    First pgm is changed

--- Firstpgm.java
+++ Firstpgm.java
@@ -1,7 +1,7 @@
 public class Firstpgm
 {
 		public static void main (String args[])
-		System.out.println("heyy rahul");
+		System.out.println("heyy");
 	for(int i=0;i<10;i++)
 	{
 		System.out.println("good eveningg folkss");
